ed8579813c3ae99f9402b7667cea52a26b4c6f8a
Mark Francis <mark@moonstar.ai>
2024-12-02 11:45:12 +0000

    Update root route

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,32 +1,34 @@
 import { createRootRoute } from '@tanstack/react-router';
 import { Outlet, ScrollRestoration } from '@tanstack/react-router';
-import { Body, Head, Html, Meta, Scripts } from '@tanstack/start';
+import { Meta, Scripts } from '@tanstack/start';
 import * as React from 'react';
 import appCss from '~/app.css?url';
 
 export const Route = createRootRoute({
-  links: () => [
-    {
-      rel: 'stylesheet',
-      href: appCss,
-    },
-  ],
-  meta: () => [
-    {
-      charSet: 'utf-8',
-    },
-    {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1',
-    },
-    {
-      title: 'New React App',
-    },
-    {
-      name: 'description',
-      content: 'Welcome to your new app!',
-    },
-  ],
+  head: () => ({
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'New React App',
+      },
+      {
+        name: 'description',
+        content: 'Welcome to your new app!',
+      },
+    ],
+  }),
   component: RootComponent,
 });
 
@@ -40,15 +42,15 @@ function RootComponent() {
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <Html>
-      <Head>
+    <html>
+      <head>
         <Meta />
-      </Head>
-      <Body>
+      </head>
+      <body>
         {children}
         <ScrollRestoration />
         <Scripts />
-      </Body>
-    </Html>
+      </body>
+    </html>
   );
 }
