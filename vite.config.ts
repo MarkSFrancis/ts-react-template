9af541e70b4be0c5e7343862029fb7ce45bb0bbd
Mark Francis <mark@moonstar.ai>
2025-06-24 13:11:39 +0100

    Switch from Vinxi to Vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+  ],
+});
