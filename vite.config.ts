7c31acdb55f7a6fdb727217bf89a5b564db4e6e5
Mark Francis <mrkfrancis8@gmail.com>
2026-04-11 12:16:33 +0100

    Drop postcss + update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 import { playwright } from '@vitest/browser-playwright';
 
 export default defineConfig({
-  plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart(),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackStart(), react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     projects: [
       {
